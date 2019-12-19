32999e7216f7e1909b597eefa8f903a7fd05442f
juliedundas <48326903+juliedundas@users.noreply.github.com>
2019-12-18 20:24:55 -0700

    Add files via upload

--- PyPoll/Main/main.py
+++ PyPoll/Main/main.py
@@ -4,9 +4,10 @@ import csv
 # Path to collect data election data csv file
 election_csv = os.path.join('..', 'Resources', 'election_data.csv')
 
-votes = {}
+vote_data = {}
 total_votes = 0
-winner = ""
+winning_votes = 0
+winning_candidate = ""
 
 with open(election_csv, 'r') as csvfile:
 
@@ -21,34 +22,41 @@ with open(election_csv, 'r') as csvfile:
 
         total_votes = total_votes + 1
 
-        if candidate in votes:
-            votes[candidate] = votes[candidate] + 1
-        else:
-            votes[candidate] = 1
+        if total_votes > total_votes:
+            winner = total_votes
 
-        if votes[candidate] > votes[candidate]:
-            votes[candidate] = winner
+        if candidate in vote_data:
+            vote_data[candidate] = vote_data[candidate] + 1
+        else:
+            vote_data[candidate] = 1
 
 #print(f"Total Votes: {total_votes}")
 print("Election Results")
-print("-----------------------")
-print(f"Total Votes: ${total_votes}")
-print("-----------------------")
+print("-" * 30)
+print(f"Total Votes: {total_votes}")
+print("-" * 30)
 #for vote, num in votes.items():
 #print(candidate + ": " + str(num))
-for candidate, votes in votes.items():
+for candidate, votes in vote_data.items():
     # print(candidate + ":   " + str(round((votes / total_votes) * 100, 2)) +"%   " + str(votes))
     #file.write(candidate + ":   " +
     #str(round((votes / total_votes) * 100, 2)) + "%   " +str(votes) + "\n")
     #winner = sum(total_votes)
 
+    #Create if statement to find the winning candidate
+    if votes > winning_votes:
+        winning_votes = votes
+        winning_candidate = candidate
+
+#Loop though and print each candidate with the percent of votes and the total votes
     print(
         str(candidate) + ": " + str(round((votes / total_votes) * 100, 3)) +
         "% " + str(votes))
 
-print(winner)
+#winner = candidate.max()
+#print(winning_candidate)
 
-print("-----------------------")
-
-print(f"Winner: {winner}")
-print("-----------------------")
+print("-" * 30)
+#Print the winning candidate
+print(f"Winner: {winning_candidate}")
+print("-" * 30)
